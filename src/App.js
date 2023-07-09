7491a138763342678db64e3a7d4761327f21730a
Anne Whitman <aniel928@gmail.com>
2023-07-09 13:57:08 -0400

    fetch gifs using fetch and api key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchForm from './Components/SearchForm';
 import GifList from "./Components/GifList";
 
 function App() {
+    const [gifs, setGifs] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.giphy.com/v1/gifs/trending?api_key=hpJTDJ7Ploz7Ax17GAawRL5VgMhqk6Km&limit=24&rating=g')
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((resp) => {
+                setGifs(resp.data);
+                console.log(resp.data);
+            })
+            .catch((err) => {
+                console.log("Error fetching and parsing data", err);
+            });
+    }, [])
+
     return (
         <div>
             <div className="main-header">
